a1ff3e2593249feefcf28657325395fb2c1da396
ali can yücel <yucelalican@hotmail.com>
2022-10-01 04:06:46 +0300

    EticaretDemoNet6

---
+++ EticaretDemo/EticaretDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using EticaretDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EticaretDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Login login)
+        {
+           if(login.Email=="yucelalican@hotmail.com" && login.Password=="1")
+            {
+                return RedirectToAction("Home","Home");
+            }
+            TempData["hata"] = "şifre veya parola hatali";
+            return RedirectToAction("Index","Home");
+        }
+
+        [HttpGet]
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ EticaretDemo/EticaretDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EticaretDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
